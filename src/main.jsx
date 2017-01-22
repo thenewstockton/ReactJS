dadda9d84dc55c6343e13735c34eacb9f396948d
KoM82U <phillip5914619@gmail.com>
2017-01-22 18:48:00 +0800

    Implemented react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,38 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ListManager = require('./components/ListManager.jsx');
+var Routes = require('./Routes.jsx');
 
-ReactDOM.render(
-    <ListManager title="Ingredients" />, document.getElementById('ingredients')
-);
+ReactDOM.render(Routes, document.getElementById('main'));
 
 
-ReactDOM.render(
-    <ListManager title="To-Do" />, document.getElementById('todo')
-);
 
-ReactDOM.render(
-    <ListManager title="Christmas" headingColor="#b31217"/>, document.getElementById('xmas')
-);
 
+
+
+
+
+
+
+
+
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var ListManager = require('./components/ListManager.jsx');
+// 
+// ReactDOM.render(
+//     <ListManager title="Ingredients" />, document.getElementById('ingredients')
+// );
+// 
+// 
+// ReactDOM.render(
+//     <ListManager title="To-Do" />, document.getElementById('todo')
+// );
+// 
+// ReactDOM.render(
+//     <ListManager title="Christmas" headingColor="#b31217"/>, document.getElementById('xmas')
+// );
+
+//////////////////////////////////////////////////
 // var React = require('react');
 // var ReactDOM = require('react-dom');
 // var List = require('./components/List.jsx');
